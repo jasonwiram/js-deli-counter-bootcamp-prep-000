0a3609ce1c48af40e816ea54f3972c5b31cb5f2c
Jason Wiram <jwiram@gmail.com>
2018-10-22 18:31:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function currentLine(line) {
     var lineString = 'The line is currently: '
     for (var i = 0; i < line.length; i++) {
       lineString = lineString + `${i+1}. ${line[i]}`
-      if (i < line.length) {
+      if (i < line.length - 1) {
         lineString += ', '
       }
     }

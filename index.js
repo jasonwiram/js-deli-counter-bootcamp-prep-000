02b750a08fe9b897efba0e9184b91b54dd3d4e1b
Jason Wiram <jwiram@gmail.com>
2018-10-22 18:24:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ function nowServing(line) {
 }
 
 function currentLine(line) {
-  var lineString = 'The line is currently'
   if (line.length === 0) {
     return "The line is currently empty."
   } else {
+    var lineString = 'The line is currently: '
     for (var i = 0; i < line.length; i++) {
       lineString = lineString + `${i+1}. ${line[i]}`
     }

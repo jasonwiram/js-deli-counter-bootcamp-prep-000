f43f8dbc26b07a927de820868af6abe1982583d1
Jason Wiram <jwiram@gmail.com>
2018-10-22 18:07:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function currentLine(line) {
   if (line.length === 0) {
     return "The line is currently empty."
   } else {
-    var lineString = ''
     for (var i = 0; i < line.length; i++) {
       
     }
   }
+  return `The line is currently:`
 }
